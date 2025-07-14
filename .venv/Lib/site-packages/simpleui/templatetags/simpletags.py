0c0c24e29f7aa4e1981b64b86ea3a331f7c4e8ef
zmyhurtzyh <zmy350186@gmail.com>
2025-07-14 16:10:50 +0800

    first commit

---
+++ .venv/Lib/site-packages/simpleui/templatetags/simpletags.py
@@ -0,0 +1,278 @@
+# -*- coding: utf-8 -*-
+
+import django
+from django import template
+from django.utils.html import format_html
+from django.conf import settings
+from django.utils.safestring import mark_safe
+
+from django.templatetags import static
+
+import os
+import json
+
+import platform
+import socket
+
+import simpleui
+
+import base64
+import time
+
+from django.db import models
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def context_test(context):
+    print(context)
+    pass
+
+
+# context.get('cl').filter_specs[1].links
+@register.simple_tag(takes_context=True)
+def load_dates(context):
+    data = {}
+    cl = context.get('cl')
+    if cl.has_filters:
+        for spec in cl.filter_specs:
+            field = spec.field
+            field_type = None
+            if isinstance(field, models.DateTimeField):
+                field_type = 'datetime'
+            elif isinstance(field, models.DateField):
+                field_type = 'date'
+            elif isinstance(field, models.TimeField):
+                field_type = 'time'
+
+            if field_type:
+                data[field.name] = field_type
+    context['date_field'] = data
+
+    return '<script type="text/javascript">var searchDates={}</script>'.format(json.dumps(data))
+
+
+@register.filter
+def get_date_type(spec):
+    field = spec.field
+    field_type = ''
+    if isinstance(field, models.DateTimeField):
+        field_type = 'datetime'
+    elif isinstance(field, models.DateField):
+        field_type = 'date'
+    elif isinstance(field, models.TimeField):
+        field_type = 'time'
+
+    return field_type
+
+
+@register.filter
+def test(obj):
+    print(obj)
+    # pass
+    return ''
+
+
+@register.filter
+def to_str(obj):
+    return str(obj)
+
+
+@register.filter
+def date_to_json(obj):
+    return json.dumps(obj.date_params)
+
+
+@register.simple_tag(takes_context=True)
+def home_page(context):
+    '''
+    处理首页，通过设置判断打开的是默认页还是自定义的页面
+    :return:
+    '''
+    home = __get_config('SIMPLEUI_HOME_PAGE')
+    if home:
+        context['home'] = home
+
+    title = __get_config('SIMPLEUI_HOME_TITLE')
+    if not title:
+        title = '首页'
+
+    icon = __get_config('SIMPLEUI_HOME_ICON')
+    if not icon:
+        icon = 'el-icon-menu'
+
+    context['title'] = title
+    context['icon'] = icon
+
+    return ''
+
+
+def __get_config(name):
+    value = os.environ.get(name, getattr(settings, name, None))
+
+    return value
+
+
+@register.filter
+def get_config(key):
+    return __get_config(key)
+
+
+@register.simple_tag
+def get_app_info():
+    dict = {
+        'version': simpleui.get_version()
+    }
+
+    return format_table(dict)
+
+
+def format_table(dict):
+    html = '<table class="simpleui-table"><tbody>'
+    for key in dict:
+        html += '<tr><th>{}</th><td>{}</td></tr>'.format(key, dict.get(key))
+    html += '</tbody></table>'
+    return format_html(html)
+
+
+@register.simple_tag(takes_context=True)
+def menus(context):
+    data = []
+
+    # return request.user.has_perm("%s.%s" % (opts.app_label, codename))
+
+    config = get_config('SIMPLEUI_CONFIG')
+
+    app_list = context.get('app_list')
+    for app in app_list:
+        _models = [
+            {
+                'name': str(m.get('name')),
+                'icon': get_icon(m.get('object_name')),
+                'url': m.get('admin_url'),
+                'addUrl': m.get('add_url'),
+                'breadcrumbs': [str(app.get('name')), str(m.get('name'))]
+            }
+            for m in app.get('models')
+        ] if app.get('models') else []
+
+        module = {
+            'name': str(app.get('name')),
+            'icon': get_icon(app.get('app_label'), str(app.get('name'))),
+            'models': _models
+        }
+        data.append(module)
+
+    # 如果有menu 就读取，没有就调用系统的
+    key = 'system_keep'
+    if config and 'menus' in config:
+        if key in config and config.get(key) != False:
+            temp = config.get('menus')
+            for i in temp:
+                data.append(i)
+        else:
+            data = config.get('menus')
+
+    return '<script type="text/javascript">var menus={}</script>'.format(json.dumps(data))
+
+
+def get_icon(obj, name=None):
+    temp = get_config_icon(name)
+    if temp != '':
+        return temp
+
+    _dict = {
+        'auth': 'fas fa-shield-alt',
+        'User': 'far fa-user',
+        'Group': 'fas fa-users-cog'
+    }
+    temp = _dict.get(obj)
+    if not temp:
+        _default = __get_config('SIMPLEUI_DEFAULT_ICON')
+        if _default is None or _default:
+            return 'far fa-file'
+        else:
+            return ''
+    return temp
+
+
+# 从配置中读取图标
+def get_config_icon(name):
+    _config_icon = __get_config('SIMPLEUI_ICON')
+    if _config_icon is None:
+        return ''
+
+    if name in _config_icon:
+        return _config_icon.get(name)
+    else:
+        return ''
+
+
+@register.simple_tag(takes_context=True)
+def load_message(context):
+    messages = context.get('messages')
+    array = [dict(msg=msg.message, tag=msg.tags) for msg in messages] if messages else []
+
+    return '<script type="text/javascript"> var messages={}</script>'.format(array)
+
+
+@register.simple_tag(takes_context=True)
+def context_to_json(context):
+    json_str = '{}'
+
+    return mark_safe(json_str)
+
+
+@register.simple_tag()
+def get_language():
+    return settings.LANGUAGE_CODE.lower()
+
+
+@register.filter
+def get_language_code(val):
+    return settings.LANGUAGE_CODE.lower()
+
+
+def get_analysis_config():
+    val = __get_config('SIMPLEUI_ANALYSIS')
+    if not val and val == False:
+        return False
+    return True
+
+
+@register.simple_tag(takes_context=True)
+def load_analysis(context):
+    try:
+        if get_analysis_config() == False:
+            return ''
+
+        # 理论上值一天只上报一次
+        key = 'simpleui_' + time.strftime('%Y%m%d', time.localtime())
+
+        if key in context.request.session:
+            return ''
+
+        b64 = ""
+        j = {
+            "n": platform.node(),
+            "o": platform.platform(),
+            "p": platform.python_version(),
+            "d": django.get_version(),
+            "s": simpleui.get_version(),
+        }
+        if 'theme_name' in context.request.COOKIES:
+            j['t'] = context.request.COOKIES['theme_name']
+        else:
+            j['t'] = 'Default'
+
+        b64 = base64.b64encode(str(j).encode('utf-8'))
+
+        url = '//simpleui.88cto.com/analysis'
+        b64 = b64.decode('utf-8')
+        html = '<script async type="text/javascript" src="{}/{}"></script>'.format(url, b64);
+        context.request.session[key] = True
+
+        return mark_safe(html)
+    except:
+        return ''
---
+++ readme.txt
@@ -0,0 +1,84 @@
+项目介绍：
+本推荐系统采用的是分层模型设计思想，第一层为前端页面模型设计，注重为实现页面的展示效果，主用的编程语言为JavaScript,和前端主流框架bootstrap。
+第二层为后端模型设计，编程语言选了简单易懂的python，用Django作为后端框架进行开发，此框架是python web系统开发的首选框架，简单易用。
+第三层为算法的设计与实现的逻辑，用协同过滤算法来实现，第四层为数据库表的设计，用SQLite数据库。
+本系统web端的功能模块，主要实现影片显示、影片分类显示、热门影片排序显示、收藏影片排序显示、时间排序显示、评分排序显示、算法推荐、影片搜索、影片信息管理等功能，并进行数据库的详细设计，完成设计阶段的各项功能，并对此系统进行功能测试，最后，系统进行相关的实际应用操作，通过系统的使用，用户进入电影推荐系统，此系统可以根据用户对电影所打的标签行为，给用户推荐用户所感兴趣的电影，准确率在百分之75左右，用户可以查看信息，观看影片，给影片评分等操作，本系统基本上完成了预期的基本功能。
+
+电影推荐系统运行说明书
+安装运行方法
+1.解压项目，用pycharm打开manage.py。
+2.在pycharm配置python解释器，(python的版本3.6.x-3.7)。
+3.打开终端输入pip install -r requirements.txt 若提示无pip。去下载get-pip.py 运行python get-pip.py
+4.安装成功后，使用pip list 查看已经安装好的package，和requirements.txt中的进行对比，确认安装无误。
+有两种运行方法:
+1.通过终端运行: python manage.py runserver
+2.通过Pycharm配置运行，右上角选中movie，一键运行，第一次运行时间可能比较长，因为需要将item_recommend的相似矩阵计算出来。将模型保存到本地。
+技术介绍：
+前端: bootstrap3 + js+ jquery
+后端: django 2.2.1 +  django--rest-framework  (MVC框架)
+数据库: sqlite3
+算法:协同过滤推荐算法
+豆瓣数据集
+通过爬虫去抓取豆瓣电影网站的电影信息，带有图片
+id,title ,image-link ,country ,years ,director-description,leader,star ,
+description,alltags,imdb,language,time-length
+电影数量: 2250
+功能介绍：
+电影展示，标签分类，详情介绍，电影搜索
+用户的登录，注册，修改信息
+用户对电影的打分，收藏，评论
+基于user和Item的协同过滤推荐算法
+后台管理系统
+
+算法简介
+冷启动问题解决
+推荐值: 相似度*评分 根据用户点赞过得商品来寻找相似度推荐。计算每个点赞过的物品和所有未点赞物品之间的得分。得分=相似度*打分值分越高表示越相似。 然后返回结果。
+项目文件介绍
+media/ 静态文件存放处，图片
+movie/ Django的默认app，负责设置的配置还有url路由,部署等功能
+static/ css文件和js文件的存放处
+user/ 主app，程序的所有代码基本都在这下面 user/migrations为自动生成的数据库迁移文件 user/templates为前端页面模板文件， user/admins.py 为管理员后台代码 user/forms.py为前端表单代码 user/models.py为数据库orm模型 user/serializers.py为restful文件，不用管。 user/urls为路由注册文件。 user/views为负责处理前端请求和与后端数据库交互的模块，也就是controller模块。
+cache_keys.py为缓存的key值名称存放文件，不用管。
+db.sqlite3数据库文件
+douban_crawler.py 豆瓣爬虫文件
+manage.py 运行的主程序，从这里启动
+Populate movies script.py 填充电影数据到数据库中
+Populate user rate.py 随机生成用户评分
+recommend_movies.py为推荐算法的部分
+model: recommend模型存放的位置
+后台管理
+通过创建管理员进入后台，已经自带管理员用户名和密码均为admins。
+同时可以通过 python manage.py createsuperuser 在终端交互输入用户名和密码即可
+创建超级管理员, (密码输入时终端暂时显示)
+进入后台: 127.0.0.1:8000/admin
+
+三、各功能代码位置介绍：
+标签分类: 数据库设计Movie通过外键关联Tags表，
+电影搜索: 在views.py search方法中。通过电影名，导演名，介绍去进行关键字搜索。
+后台管理: 通过django自带的admin后台加插件 在admins.py中注册数据库模型
+两种推荐算法: 都在recommend_movies.py文件中。
+推荐显示的代码:
+前端: items.html
+后端: views.py中 388行 user_recommend 传递数据到前端template
+算法：recommend_movies.py。
+依赖包和框架版本
+(venv) E:\mysqlproject>pip list
+Package             Version
+aiohttp             3.7.4.
+asgiref             3.3.4
+async-timeout       3.0.1
+attrs               21.2.0
+beautifulsoup4      4.9.3
+bs4                 0.0.1
+certifi             2020.12.5
+chardet             3.0.4
+crawler             0.0.2
+Django              2.2.10
+django-simpleui     2.1
+djangorestframework 3.9.1
+greenlet            1.1.0
+idna                2.10
+idna-ssl            1.1.0
+importlib-metadata  4.0.1
+lxml                4.6.3
+multidict           5.1.0
